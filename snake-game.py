fb1c500b1f5c45591b252c141f083043bce05435
Jakoub <118178881+Jak0ub@users.noreply.github.com>
2025-03-20 18:41:18 +0100

    Add files via upload

--- snake-game.py
+++ snake-game.py
@@ -2,6 +2,15 @@ import pygame
 from pygame.locals import *
 import random
 import time
+import os
+import platform
+#Nastavení cesty, kam se bude ukládat soubor s nejvyšším skórem
+platform_system = platform.system()
+if platform_system == "Windows":
+	temp_Windows_path = os.environ.get('TEMP')
+	soubor_skóre = f"{temp_Windows_path}/high_score-snake.txt"
+else:
+	soubor_skóre = "/tmp/high_score-snake.txt"
 #inicializace
 pygame.init()
 #Okno
@@ -16,7 +25,7 @@ fps = 10
 run = True
 prohra = 0
 stisk = 0
-obtiznosti = ["easy", "medium", "hard"]
+obtiznosti = ["Snadná", "Střední", "Obtížná"]
 obtiznost = obtiznosti[2] #volba obtížnosti
 
 aktualizuj = False
@@ -37,6 +46,7 @@ nove_jidlo = True
 prodlouzeny_block = [0, 0]
 #font
 font = pygame.font.SysFont(None, 40)
+font_mensi = pygame.font.SysFont(None, 33)
 #Barvy
 bg = (0, 0, 0)
 body_inner = (255, 255, 255)
@@ -92,6 +102,83 @@ def vykresleni_konce_hry():
     znovu = font.render(znovu, True, (255, 255, 255))
     pygame.draw.rect(okno, (255, 0, 0), tlačítko, 2, 3)
     okno.blit(znovu, (šířka_okna // 2 - 80, výška_okna // 2 + 10))
+    info = "<--- klikni"
+    info = font_mensi.render(info, True, (0, 255, 0))
+    okno.blit(info, (šířka_okna // 2 + 100, výška_okna // 2 + 10))
+    if změna == True:
+        info = "|i| Dosáhl jsi nového nejlepšího skóre |i|"
+        info = font_mensi.render(info, True, (255, 255, 255))
+        okno.blit(info, (0 + šířka_okna // 6, výška_okna - výška_okna // 5))
+
+
+#Zapisování nejlepšího výsledku skóre uživatelem dosaženého
+def zápis_score():
+    #  Struktura souboru pro high score:
+    #  1;10 
+    #  2;8
+    #  3;12
+    # čísla značí obtížnost, to po ";" jsou hodnoty nejlepšího skóre pro individuální level obtížnosti 
+    high_score = []
+    working = score #pracovní proměná pro lepší orientaci
+    vytvoř = False
+    změna = False
+    try:
+        with open(f"{soubor_skóre}", "r", encoding="utf-8") as file:
+            radky = file.readlines()
+            try:
+                for i in range(3):
+                    hlavni = radky[i].strip()
+                    try:
+                        if int(hlavni.split(";")[0]) == i + 1:
+                            try:
+                                cislo = hlavni.split(";")[1]
+                                try:
+                                    cislo = int(cislo)
+                                    high_score.append(cislo)
+                                except ValueError:
+                                    high_score.append("0")
+                            except IndexError:
+                                high_score.append("0")
+                        else:
+                            vytvoř = True
+                    except ValueError:
+                        vytvoř = True
+                        high_score = ["0", "0", "0"]
+            except IndexError:
+                vytvoř = True
+                high_score = ["0", "0", "0"]
+    except FileNotFoundError:
+        vytvoř = True
+        high_score = ["0", "0", "0"]
+    if vytvoř == True:
+        high_score[obtiznosti.index(obtiznost)] = working
+        radky = []
+        pocet = 0
+        for obsah in high_score:
+            pocet += 1
+            if pocet == 3:
+                radky.append(f"{pocet};{obsah}")
+            else:
+                radky.append(f"{pocet};{obsah}\n")
+        with open(f"{soubor_skóre}", "w", encoding="utf-8") as file:
+            file.writelines(radky)
+        if working != 0:
+            změna = True
+    if int(high_score[obtiznosti.index(obtiznost)]) < working:
+        high_score[obtiznosti.index(obtiznost)] = working
+        radky = []
+        pocet = 0
+        for obsah in high_score:
+            pocet += 1
+            if pocet == 3:
+                radky.append(f"{pocet};{obsah}")
+            else:
+                radky.append(f"{pocet};{obsah}\n")
+        with open(f"{soubor_skóre}", "w", encoding="utf-8") as file:
+            file.writelines(radky) 
+        změna = True 
+
+    return změna
 #Volba obtížnosti
 run = True
 prohra = 0
@@ -100,7 +187,19 @@ start = time.time()
 easy = [šířka_okna // 5, výška_okna // 3]
 medium = [šířka_okna // 2, výška_okna // 3]
 hard = [šířka_okna // 1.25, výška_okna // 3]
+def popisky():#Popisky k blockům obtížnosti, volitelné
+    moznosti = [easy, medium, hard]
+    for i in range(3):
+        info = f"{obtiznosti[i]}"
+        block = moznosti[i]
+        info = font_mensi.render(info, True, (255, 255, 255))
+        okno.blit(info, (block[0] + 30, block[1]))
+skore = [4 * velikost_blocku, (výška_okna // 3) * 2 + velikost_blocku * 3]
+reset = [šířka_okna // 1.5, (výška_okna // 3) - velikost_blocku * 3]
+popis = [šířka_okna - velikost_blocku * 4, (výška_okna // 3) * 2 + velikost_blocku * 3]
 def choice():
+    timeout = 0
+    zobraz_skore = False
     run = True
     start = time.time()
     pozice_hada = [[int(šířka_okna / 2), int(výška_okna / 2)]]#list pozic blocků + přidání prvního blocku
@@ -109,12 +208,13 @@ def choice():
     pozice_hada.append([int(šířka_okna / 2) - velikost_blocku * 3, int(výška_okna / 2)])#Čtvrtý block
     konec_hry = False
     aktualizuj = False
+    cekej = 0
     směr = 2
+    pomoc = False
     prohra = 0
     while run:
         cas = round(time.time() - start, 2)
         vykreslení_okna()
-        vykresleni_volby()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -128,19 +228,77 @@ def choice():
                 if event.key == pygame.K_a and směr != 2:#Pokud stiskneme A a proměnná směr není 2, což by znamenalo že had se pohybuje směrem doprava, tak se změní proměnná směr na 1, což znamená, že pojede doleva
                     směr = 4
                 stisk += 1
-        
-        pygame.draw.rect(okno, (0, 255, 0), (easy[0], easy[1], velikost_blocku, velikost_blocku))
-        pygame.draw.rect(okno, (255, 255, 0), (medium[0], medium[1], velikost_blocku, velikost_blocku))
-        pygame.draw.rect(okno, (255, 0, 0), (hard[0], hard[1], velikost_blocku, velikost_blocku))
-        if pozice_hada[0] == easy:#[0] je block hlavy hada a jeho pozice, neboli (1, 20) třeba. Pokud se rovná s pozicí jídla, tak se podmínka vykoná
-            obtiznost = obtiznosti[0]
-            break
-        elif pozice_hada[0] == medium:#[0] je block hlavy hada a jeho pozice, neboli (1, 20) třeba. Pokud se rovná s pozicí jídla, tak se podmínka vykoná
-            obtiznost = obtiznosti[1]
-            break
-        elif pozice_hada[0] == hard:#[0] je block hlavy hada a jeho pozice, neboli (1, 20) třeba. Pokud se rovná s pozicí jídla, tak se podmínka vykoná
-            obtiznost = obtiznosti[2]
-            break
+        if zobraz_skore == False:
+            vykresleni_volby()
+            if pomoc == True:
+                popisky()
+            pygame.draw.rect(okno, (0, 255, 0), (easy[0], easy[1], velikost_blocku, velikost_blocku))
+            pygame.draw.rect(okno, (255, 255, 0), (medium[0], medium[1], velikost_blocku, velikost_blocku))
+            pygame.draw.rect(okno, (255, 0, 0), (hard[0], hard[1], velikost_blocku, velikost_blocku))
+            pygame.draw.rect(okno, (0, 255, 255), (skore[0], skore[1], velikost_blocku, velikost_blocku))#Block pro volbu zobrazení nejvyššího dosaženého skóre
+            pygame.draw.rect(okno, (5, 110, 80), (popis[0], popis[1], velikost_blocku, velikost_blocku))
+            info = "Nápověda"
+            info = font_mensi.render(info, True, (255, 255, 255))
+            okno.blit(info, (popis[0] - 40, popis[1] + 30))
+            info = "Nejvyšší skóre"
+            info = font_mensi.render(info, True, (255, 255, 255))
+            okno.blit(info, (skore[0] - 64, skore[1] + 30))
+            if pozice_hada[0] == easy:#[0] je block hlavy hada a jeho pozice, neboli (1, 20) třeba. Pokud se rovná s pozicí jídla, tak se podmínka vykoná
+                obtiznost = obtiznosti[0]
+                break
+            elif pozice_hada[0] == medium:#[0] je block hlavy hada a jeho pozice, neboli (1, 20) třeba. Pokud se rovná s pozicí jídla, tak se podmínka vykoná
+                obtiznost = obtiznosti[1]
+                break
+            elif pozice_hada[0] == hard:#[0] je block hlavy hada a jeho pozice, neboli (1, 20) třeba. Pokud se rovná s pozicí jídla, tak se podmínka vykoná
+                obtiznost = obtiznosti[2]
+                break
+            elif pozice_hada[0] == skore:
+                if round(time.time() - timeout, 2) > 0.3:
+                    zobraz_skore = True
+                    timeout = time.time()
+            elif pozice_hada[0] == popis:
+                if round(time.time() - cekej, 2) > 0.3:
+                    cekej = time.time()
+                    if pomoc == False:
+                        pomoc = True
+                    else:
+                        pomoc = False
+                else:
+                    cekej = time.time()
+        else:
+            info = "Reset hodnot"
+            info = font_mensi.render(info, True, (255, 255, 255))
+            okno.blit(info, (reset[0] + 30, reset[1]))
+            pygame.draw.rect(okno, (255, 0, 0), (reset[0], reset[1], velikost_blocku, velikost_blocku))
+            info = "Exit"
+            info = font_mensi.render(info, True, (255, 255, 255))
+            okno.blit(info, (skore[0] - 15, skore[1] + 30))
+            if round(time.time() - timeout, 2) > 0.3:
+                pygame.draw.rect(okno, (0, 255, 255), (skore[0], skore[1], velikost_blocku, velikost_blocku))
+                if pozice_hada[0] == skore:
+                    zobraz_skore = False
+                    timeout = time.time()
+                if pozice_hada[0] == reset:
+                    with open(f"{soubor_skóre}", "w", encoding="utf-8") as file: 
+                        file.write("1;0\n2;0\n3;0")
+            hodnoty = []
+            try:
+                with open(f"{soubor_skóre}", "r", encoding="utf-8") as file:
+                    radky = file.readlines()
+                    for i in range(3):
+                        try:
+                            hodnoty.append(radky[i].split(";")[1].strip())
+                        except IndexError:
+                            hodnoty.append("0")
+            except FileNotFoundError:
+                hodnoty = ["0", "0", "0"]
+            for i in range(3):
+                hodnota = f"{obtiznosti[i]} : {hodnoty[i]}"
+                hodnota = font_mensi.render(hodnota, True, (255, 255, 255))
+                okno.blit(hodnota, (50, (výška_okna // 8) * (2 + i)))
+                box = Rect(25, (výška_okna // 8) * (2 + i) - 11, 160, 45)
+                pygame.draw.rect(okno, (255, 0, 0), box, 2, 3)
+
         
         if cas > 1 / fps:
             aktualizuj = True
@@ -198,10 +356,10 @@ def choice():
                     hlava = 0
         #update displaye
         pygame.display.update()
-    return obtiznost
+    return obtiznost, pozice_hada, směr
 #Main loop
 start = time.time()
-obtiznost = choice()
+obtiznost, pozice_hada, směr = choice()
 while run:
     cas = round(time.time() - start, 2)
     vykreslení_okna()
@@ -291,9 +449,11 @@ while run:
         prohra += 1
         if prohra == 1:
             pygame.time.delay(2000)
+            změna = zápis_score()
         okno.fill((0, 0, 0))
         vykresleni_konce_hry()
         vykreslení_score()
+        
         if event.type == pygame.MOUSEBUTTONDOWN and kliknuto == False:#Kliknutní
             kliknuto = True
         if event.type == pygame.MOUSEBUTTONUP and kliknuto == True:#Upuštění
@@ -315,7 +475,7 @@ while run:
                 stisk = 0
                 prohra = 0
                 konec_hry = False
-                obtiznost = choice()
+                obtiznost, pozice_hada, směr = choice()
     hlava = 1#Proměnná, která nám zajistí, že podmínka ve for loopu níže bude splněna pouze jednou, pro získání jiné barvy pro block představující hlavu
     if konec_hry == False or prohra == 0:
         for x in pozice_hada:
